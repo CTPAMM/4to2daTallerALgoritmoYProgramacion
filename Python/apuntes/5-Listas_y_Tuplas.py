ea516aadc6807f0a55f59555ee91b99e118c1eac
Pablo <gpabloandres@gmail.com>
2023-08-06 16:34:55 -0300

    Actualiza repo

--- Python/apuntes/5-Listas_y_Tuplas.py
+++ Python/apuntes/5-Listas_y_Tuplas.py
@@ -1,5 +1,5 @@
 #----------------------------------------------------------
-# Fecha: 02-08-23
+# Fecha: 31-07-23
 # Estudiante: Su Nombre Completo
 # Título: Listas y Tuplas.
 #----------------------------------------------------------
@@ -7,30 +7,34 @@
 #---------------------------------------------------------------------
 #  LISTAS
 #---------------------------------------------------------------------
-# Declarar y definir las variables
-lista_marcas = ['vw', 'Pagani', 'Mitshubishi', 'Lexus']
-print(lista_marcas) 
 
-# Imprimir la lista con un bucle FOR
+# Declarar y definir las variables
+lista_marcas = ['Fiat', 'Ford', 'Chevrolet', 'Mercedez Benz']
+print(lista_marcas)
+
+# Bucle FOR para recorrer los elementos de la lista
 for marca in lista_marcas:
     print(marca)
-    
+
 # Imprimir un elemento concreto de la lista
-print(f"La marca es: {lista_marcas[2]}")
+print(f"El segundo elemento de la lista es: {lista_marcas[1]}")
 
 # Reemplazar un elemento de la lista
-lista_marcas[2] = 'Toyota'
+lista_marcas[3] = 'Audi'
 print(lista_marcas)
 
 # Agregar un elemento en la lista en la última posición
-lista_marcas.append('Fiat')
+lista_marcas.append('Toyota')
 print(lista_marcas)
 
 # Agregar un elemento en la lista en una posición concreta
-lista_marcas.insert(3, 'DS')
+lista_marcas.insert(2, 'Subaru')
 print(lista_marcas)
 
 # Eliminar un elemento concreto de la lista
-lista_marcas.pop(0)
+lista_marcas.pop(2)
 print(lista_marcas)
 
+# Ordenar alfabeticamente la lista
+lista_marcas.sort()
+print(lista_marcas)
